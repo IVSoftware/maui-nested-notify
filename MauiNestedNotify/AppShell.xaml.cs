98b3c6ef76b3ccba8ad242b8bc71b52347c695df
Tom Gregor <tgregor@ivsoftware.com>
2024-09-29 09:25:39 -0600

    changes aspect ratio of app wunning on Windows Machine.

--- MauiNestedNotify/AppShell.xaml.cs
+++ MauiNestedNotify/AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace MauiNestedNotify
+﻿#if WINDOWS
+using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Interop;
+using WinRT.Interop; // Required for window interop
+#endif
+
+
+namespace MauiNestedNotify
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        public AppShell() => InitializeComponent();
     }
 }
